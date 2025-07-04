dd2bcdf112172024c61b864d17d8b12f49fd7e22
Joseph Ibok <josephibok75@gmail.com>
2025-07-04 14:14:43 +0100

    Initial Commit: Installed App dependencies and configured Nativewindcss

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="text-2xl font-black text-blue-500">Mobile App Home Screen</Text>
+    </View>
+  );
+}
