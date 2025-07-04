1ffc56f04ddbbae0f9911f5d4d00c2aad1af0ffa
Joseph Ibok <josephibok75@gmail.com>
2025-07-04 14:25:00 +0100

    Updated README.md

--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="text-2xl font-black text-blue-500">Mobile App Home Screen</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-2xl font-black text-dark-200">Mobile App Home Screen</Text>
     </View>
   );
 }
