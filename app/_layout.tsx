50001c11b92d72b143e26fd4ebaee06b22f9a5fd
Joseph Ibok <josephibok75@gmail.com>
2025-07-06 20:53:28 +0100

    Hid App StatusBar using expo-status-bar library

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,21 @@
 import { Stack } from "expo-router";
+import { StatusBar } from 'expo-status-bar';
 import "./global.css";
 
 export default function RootLayout() {
-  return <Stack>
-    <Stack.Screen 
-      name="(tabs)"
-      options={{ headerShown: false }}
-    />
-    <Stack.Screen 
-      name="movies/[id]"
-      options={{ headerShown: false }}
-    />
-  </Stack>;
+  return (
+    <>
+      <Stack>
+        <Stack.Screen 
+          name="(tabs)"
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen 
+          name="movies/[id]"
+          options={{ headerShown: false }}
+        />
+      </Stack>
+      <StatusBar hidden={true} />
+    </>
+  )
 }
